b75238c5b2130de4c1c2f4fe4acf8e4fba80f61f
Franciszer <ftthierree@gmail.com>
2021-10-04 16:06:16 +0200

    State tests passed

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 
-mod State;
+mod state;
 
 fn main() {
+    state::SIZE.set(3).unwrap();
+    state::BOARD_SIZE.set(9).unwrap();
 }
---
+++ src/state.rs
@@ -0,0 +1,216 @@
+use std::convert::TryInto;
+use once_cell::sync::OnceCell;
+
+pub static SIZE: OnceCell<u16> = OnceCell::new();
+pub static BOARD_SIZE: OnceCell<u16> = OnceCell::new();
+
+// TODO make State.size and State.board_size static
+
+#[derive(Debug)]
+struct Point {
+    x: u16,
+    y: u16,
+}
+
+impl Point {
+    #[inline]
+    fn to_1d(&self) -> u16 {
+        self.x * SIZE.get().unwrap() + self.y
+    }
+
+    fn to_2d(idx: u16) -> Point {
+        Point {
+            x: idx / SIZE.get().unwrap(),
+            y: idx % SIZE.get().unwrap(),
+        }
+    }
+
+    fn left(&self) -> Option<Point> {
+        if self.x == 0 {
+            None
+        } else {
+            Some(Point {
+                x: self.x - 1,
+                y: self.y,
+            })
+        }
+    }
+
+    fn right(&self) -> Option<Point> {
+        if self.x >= SIZE.get().unwrap() - 1 {
+            None
+        } else {
+            Some(Point {
+                x: self.x + 1,
+                y: self.y,
+            })
+        }
+    }
+
+    fn up(&self) -> Option<Point> {
+        if self.y == 0 {
+            None
+        } else {
+            Some(Point {
+                x: self.x,
+                y: self.y - 1,
+            })
+        }
+    }
+
+    fn down(&self) -> Option<Point> {
+        if self.y >= SIZE.get().unwrap() - 1 {
+            None
+        } else {
+            Some(Point {
+                x: self.x,
+                y: self.y + 1,
+            })
+        }
+    }
+}
+
+pub struct State {
+    // state of the board
+    board: Vec<u16>,
+    // position of 0 on board
+    pos: Point,
+    // size one dimension of the board, needs static
+}
+
+impl State {
+    fn build_state(board: Vec<u16>) -> State {
+        let zero = Point::to_2d(board.iter().position(|&r| r == 0).unwrap().try_into().unwrap());
+        State {
+            board,
+            pos: zero,
+        }
+    }
+
+    fn build_child(&self, new_pos: &Point) -> State {
+        let parent_idx = self.pos.to_1d();
+        let child_idx = new_pos.to_1d();
+
+        let mut v: Vec<u16> = self.board.clone();
+        v.swap(parent_idx.into(), child_idx.into());
+        State::build_state(v)
+    }
+
+    pub fn gen_children(&self) -> [Option<State>; 4] {
+        let children_pos: [Option<Point>; 4] = [self.pos.left(), self.pos.right(), self.pos.up(), self.pos.down()];
+
+        let children: [Option<State>; 4] = children_pos.map(|el| {
+            match el {
+                Some(p) => Some(self.build_child(&p)),
+                None => None
+            }
+        });
+
+        children
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::state::Point;
+
+    #[test]
+    fn points_basic() {
+        let p = Point { x: 1, y: 1 };
+        assert_eq!(p.x, 1);
+        assert_eq!(p.y, 1);
+    }
+
+    #[test]
+    fn points_left() {
+        let p = Point { x: 1, y: 1 };
+        let p = p.left().unwrap();
+        assert_eq!(p.x, 0);
+        assert_eq!(p.y, 1);
+        let p = p.left();
+        match p {
+            Some(_) => assert!(false),
+            None => assert!(true)
+        }
+    }
+
+    #[test]
+    fn points_right() {
+        crate::state::SIZE.set(3).unwrap();
+        crate::state::BOARD_SIZE.set(9).unwrap();
+        let p = Point { x: 1, y: 1 };
+        let p = p.right().unwrap();
+        assert_eq!(p.x, 2);
+        assert_eq!(p.y, 1);
+        let p = p.right();
+        match p {
+            Some(_) => assert!(false),
+            None => assert!(true)
+        }
+    }
+
+    #[test]
+    fn points_up() {
+        crate::state::SIZE.set(3).unwrap();
+        crate::state::BOARD_SIZE.set(9).unwrap();
+        let p = Point { x: 1, y: 1 };
+        let p = p.up().unwrap();
+        assert_eq!(p.x, 1);
+        assert_eq!(p.y, 0);
+        let p = p.up();
+        match p {
+            Some(_) => assert!(false),
+            None => assert!(true)
+        }
+    }
+
+    #[test]
+    fn points_down() {
+        crate::state::SIZE.set(3).unwrap();
+        crate::state::BOARD_SIZE.set(9).unwrap();
+        let p = Point { x: 1, y: 1 };
+        let p = p.down().unwrap();
+        assert_eq!(p.x, 1);
+        assert_eq!(p.y, 2);
+        let p = p.down();
+        match p {
+            Some(_) => assert!(false),
+            None => assert!(true)
+        }
+    }
+
+    #[test]
+    fn state_build_state() {
+        use super::*;
+        SIZE.set(3).unwrap();
+        BOARD_SIZE.set(9).unwrap();
+        let s = State::build_state(vec![1, 2, 3, 4, 5, 6, 7, 0, 8]);
+        assert_eq!(s.pos.to_1d(), 7);
+        let s = State::build_state(vec![0, 2, 3, 4, 5, 6, 7, 1, 8]);
+        assert_eq!(s.pos.to_1d(), 0);
+        let s = State::build_state(vec![1, 2, 3, 4, 5, 6, 7, 8, 0]);
+        assert_eq!(s.pos.to_1d(), 8);
+    }
+
+    #[test]
+    fn state_build_child() {
+        use super::*;
+        SIZE.set(3).unwrap();
+        BOARD_SIZE.set(9).unwrap();
+        let s = State::build_state(vec![1, 2, 3, 4, 5, 6, 7, 0, 8]);
+        let s2 = s.build_child(&Point {x: 0, y: 0});
+        assert_eq!(s2.pos.to_1d(), 0);
+        assert_eq!(s2.board[s2.pos.to_1d() as usize], 0);
+        assert_eq!(s2.board[7], 1);
+    }
+
+    #[test]
+    #[should_panic]
+    fn state_build_child_p() {
+        use super::*;
+        SIZE.set(3).unwrap();
+        BOARD_SIZE.set(9).unwrap();
+        let s = State::build_state(vec![1, 2, 3, 4, 5, 6, 7, 0, 8]);
+        let _s2 = s.build_child(&Point {x: 10, y: 10});
+    }
+}
