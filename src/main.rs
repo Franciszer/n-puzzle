bcc71a8111abfa4766b47ebf9f10a8fd6a200e4e
Alexandre Seo <perso@alexandre-seo.tk>
2021-10-04 18:29:06 +0200

    Add map parser

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,27 @@
+#![allow(dead_code)] // Remove when project is somewhat finished
+use std::error::Error;
 
+mod parser;
 mod state;
 
-fn main() {
+#[derive(Debug)]
+pub struct Map {
+    size: u16,
+    board: Vec<u16>,
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let input = "4
+0  10 5  7
+11 14 4  8
+1  2  6  13
+12 3  15 9
+";
+
+    let (_, (size, board)) = parser::parse_map(input)?;
+    let map = parser::validate_map(size, board)?;
+    println!("{:?}", map);
     state::SIZE.set(3).unwrap();
     state::BOARD_SIZE.set(9).unwrap();
+    Ok(())
 }
---
+++ src/parser.rs
@@ -0,0 +1,37 @@
+use nom::bytes::complete::take_while1;
+use nom::character::complete;
+use nom::character::complete::multispace0;
+use nom::multi::{separated_list0, separated_list1};
+use nom::IResult;
+
+use crate::Map;
+
+fn consume_line(line: &str) -> IResult<&str, Vec<u16>> {
+    let (remaining, data) = separated_list1(take_while1(|c| c == ' '), complete::u16)(line)?;
+    Ok((remaining, data))
+}
+
+pub fn parse_map(map: &str) -> IResult<&str, (u16, Vec<Vec<u16>>)> {
+    let (remaining, size) = complete::u16(map)?;
+    let (remaining, _whitespace) = multispace0(remaining)?; // Remove the trailing newline after the map size
+    let (remaining, board) = separated_list0(take_while1(|c| c == '\n'), consume_line)(remaining)?;
+
+    Ok((remaining, (size, board)))
+}
+
+pub fn validate_map(size: u16, board: Vec<Vec<u16>>) -> Result<Map, &'static str> {
+    if board.len() != size as usize {
+        return Err("Board has invalid size");
+    }
+    let mut map = Map {
+        size,
+        board: Vec::with_capacity(size as usize),
+    };
+    for v in board {
+        if v.len() != size as usize {
+            return Err("Board has invalid size");
+        }
+        map.board.extend(v);
+    }
+    Ok(map)
+}
