3f22f9eaac5d28d04545f28bcd291b5f1c00ea04
Alexandre Seo <perso@alexandre-seo.tk>
2021-10-06 15:54:34 +0200

    Impl Debug for Map

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,33 @@
 #![allow(dead_code)] // Remove when project is somewhat finished
 use std::error::Error;
+use std::fmt::{Debug, Formatter};
 
 mod parser;
 mod state;
 
-#[derive(Debug)]
 pub struct Map {
     size: u16,
     board: Vec<u16>,
+}
+
+impl Debug for Map {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        let width = match self.board.iter().max() {
+            Some(w) => w.to_string().len(),
+            None => return Ok(()),
+        };
+        for line in self.board.chunks_exact(self.size as usize) {
+            for (i, e) in line.iter().enumerate() {
+                if i == self.size as usize - 1 {
+                    write!(f, "{:width$}", e, width = width)?;
+                } else {
+                    write!(f, "{:width$} ", e, width = width)?;
+                }
+            }
+            f.write_str("\n")?;
+        }
+        Ok(())
+    }
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
@@ -20,7 +40,7 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     let (_, (size, board)) = parser::parse_map(input)?;
     let map = parser::validate_map(size, board)?;
-    println!("{:?}", map);
+    print!("{:?}", map);
     state::MAP.set(map).unwrap();
     Ok(())
 }
